c69bdc34f4651d475da9e69d0d1808e629960ac9
Gowtham1234567 <gowthamadityaa@gmail.com>
2021-12-02 10:49:13 +0530

    first commit

---
+++ girtion.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.hello1;
+
+/**
+ *
+ * @author ItLab
+ */
+public class girtion {
+    public static void main(String args [])
+    {
+        int a,b,sum=0;
+        a=50;
+        b=60;
+        sum=a+b;
+        System.out.println("the sum of a,b is"+sum);
+        
+        
+    }
+}
